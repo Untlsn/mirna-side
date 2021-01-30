40683e513cf92ba27b11aca65966fc67be147b99
Untlsn <Untellson@gmail.com>
2021-01-30 17:50:10 +0100

    Thorough index remake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,41 @@
-import * as React from "react"
+import * as React from 'react'
 import styled from 'styled-components'
-import FogBackground from '../components/FogBackground'
 import '../style/global.css'
+import FogBackground from '../components/FogBackground'
+import { useMediaQuery } from 'react-responsive'
+import NavMediator from '../components/NavMediator'
+import imagesAndSideData from '../graphqlTemplates/getImagesAndSideData'
 
-const MainWrapper = styled.div`
+const FullViewWrapper = styled.div`
+  position: relative;
   background-color: #121212;
   height: 100vh;
   width: 100vw;
 `
 
-
-
 export default () => {
+  const { fogImg, logoImg, site } = imagesAndSideData()
+
+  const [fogSrc, logoSrc] = [fogImg, logoImg].map(
+    (image) => image.childImageSharp.fluid.src
+  )
+  const { title, navLinks } = site.siteMetadata
+
+  const isDesktop = useMediaQuery({
+    query: '(min-device-width: 1224px)',
+  })
 
   return (
-    <MainWrapper>
-      <FogBackground />
-    </MainWrapper>
+    <>
+      <NavMediator
+        isDesktop={isDesktop}
+        navLinks={navLinks}
+        logoSrc={logoSrc}
+        title={title}
+      />
+      <FullViewWrapper>
+        <FogBackground fogSrc={fogSrc} />
+      </FullViewWrapper>
+    </>
   )
 }
-
-
